caadbd52e7b266dc8fbbff6f9463717644cc0607
nadyamasha2004 <90305928+nadyamasha2004@users.noreply.github.com>
2021-12-04 14:00:19 +0300

    Add files via upload

---
+++ Занятие 6/Дмитриева _Акимова М/Дмитриева _Акимова М/Дмитриева _Акимова М/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Дмитриева__Акимова_М
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            int[] mas = new int[20];
+            Random rnd = new Random();
+
+
+            for (int i = 0; i < 20; i++)
+            {
+                mas[i] = rnd.Next(0,20) ;
+                if ((mas[i] % 7 == 5) || mas[i] % 7 == 1|| mas[i] % 7 == 2)
+                {
+                    Console.WriteLine("{0}", mas[i]);
+                }
+
+                else
+                {
+                    
+                }
+               
+
+        }
+    }
+    }
+}
---
+++ Занятие 7/Дмитриева_Акимова/Дмитриева_Акимова/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Дмитриева_Акимова
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("+++++");
+            Console.WriteLine("|i   |y  |");
+            for (double i = -Math.PI; i < Math.PI; i =i+ Math.PI / 4)
+            {
+                double y = Math.Cos(i) * Math.Cos(i);
+                Console.WriteLine("|{0:0.0}|{1:0.0}|",i, y);
+            }
+
+            Console.WriteLine("+++++"); 
+            Console.WriteLine("I hate you, World!");
+        }
+    }
+}
+
