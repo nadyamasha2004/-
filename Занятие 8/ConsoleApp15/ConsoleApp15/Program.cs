b7d20cf35a05bed83c4e53855dee1e69ac13be36
nadyamasha2004 <90305928+nadyamasha2004@users.noreply.github.com>
2021-12-06 14:11:14 +0300

    Add files via upload

---
+++ Занятие 8/ConsoleApp15/ConsoleApp15/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ConsoleApp15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            {
+                int[] mas = new int[12];
+                Random rnd = new Random();
+                for (int i = 0; i < 12; i++)
+                    mas[i] = rnd.Next(24);
+                for (int i = 0; i < 12; i++)
+                    Console.Write("{0}  ", mas[i]);
+                int s = 0;
+                for (int i = 0; i < 12; i++)
+                    if (i % 2 == 0) { s = s + mas[i]; }
+                Console.Write("Сумма элементов массива с четными индексами {0}", s);
+                Console.ReadKey();
+
+            }
+        }
+    }
+}
